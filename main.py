8ee1c4323c55e37c57743d4fa263a4762bf08eb1
H. Kamran <hkamran80@users.noreply.github.com>
2018-04-12 18:41:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import requests
+import getpass
+import bs
+
+subdomain = input("SchoolLoop subdomain (mvhs): ")
+username = input("Username: ")
+password = getpass.getpass()
+
+args = []
+
+args.append(subdomain)
+args.append(username)
+args.append(password)
+
+r1 = requests.get(("https://" + args[0] + ".schoolloop.com/portal/guest_home?d=x"))
+s1 = bs.bs(r1.text)
+
+form_data_id = s1.find("input", {"id": "form_data_id"})["value"]
+cookies_sent = {"JSESSIONID": r1.cookies["JSESSIONID"], "slid": r1.cookies["slid"]}
+payload = {"login_name": args[1], 'password': args[2], 'event_override': 'login', 'form_data_id': form_data_id}
+
+r2 = requests.post(("https://" + args[0] + ".schoolloop.com/portal/guest_home?etarget=login_form"), cookies=cookies_sent, data=payload)
+s2 = bs.bs(r2.text)
+
+grades = []
+
+for row in s2.find_all("table", {"class": "student_row"}):
+
+	class_name = str(row.find("td", {"class": "course"}).a.text)
+	teacher_name = str(row.find("td", {"class": "teacher co-teacher"}).a.text).strip().split(", ")[1] + " " + str(row.find("td", {"class": "teacher co-teacher"}).a.text).strip().split(", ")[0]
+
+	try:
+		grade = float(row.find("div", {"class": "percent"}).text.replace("%", ""))
+	except AttributeError:
+		grade = "No Grades Published"
+
+	grades.append([class_name, teacher_name, grade])
+
+print(grades)
